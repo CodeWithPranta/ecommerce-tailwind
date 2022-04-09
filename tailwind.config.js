a60313a8293f565d52cba1552e21e72f2af6373a
webapp99 <webapp99tube@gmail.com>
2022-04-10 02:33:09 +0600

    OrganicStore with Tailwindcss3

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+     },
+    extend: {
+    },
+  },
+  plugins: [],
+}
